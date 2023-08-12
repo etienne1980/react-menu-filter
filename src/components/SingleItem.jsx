949b6644f6bd2c2d85020320a68912dea57d2a54
etienne1980 <ducation.life@gmail.com>
2023-08-12 18:40:44 +0200

    .

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react"
 
 
 
 
 const SingleItem = ({desc, img, price, title}) => {
+
+  let [readMore, setReadMore] = useState(true)
 
   return (
 
@@ -17,7 +20,16 @@ const SingleItem = ({desc, img, price, title}) => {
                 <span className="item-price">{price}</span>
             </header>
 
-            <p className="item-text">{desc}</p>
+            <p className="item-text">
+              
+              {/* display a substring based on the state value (conditional rendering) */}
+              {readMore ? desc.substring(0,50) : desc }
+
+              {/* display the text of the button based on state value */}
+              <button onClick={() =>{setReadMore(!readMore)}} className="read-more" type="button">{readMore ? '... read more' : '... less'}</button>
+
+
+            </p>
 
         </div>
 
